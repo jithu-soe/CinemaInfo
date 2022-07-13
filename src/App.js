d0bcc71f0a43a7ba44892e3122c2d8523c8c8e42
jithu-soe <jithusoe@gmail.com>
2022-07-13 11:25:51 +0530

    finalise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { BrowserRouter, Route,Routes} from 'react-router-dom';
+import { BrowserRouter, Route,Routes, Switch} from 'react-router-dom';
 
 import Title from "./Title.js"
 import Footer from "./Footer.js"
