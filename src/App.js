16e822ccc5d862f9c598e1e4ecd5dd961daabf13
jithu-soe <jithusoe@gmail.com>
2022-07-13 11:29:11 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { BrowserRouter, Route,Routes, Switch} from 'react-router-dom';
+import { BrowserRouter, Route,Routes} from 'react-router-dom';
 
 import Title from "./Title.js"
 import Footer from "./Footer.js"
@@ -17,11 +17,11 @@ const App = () => {
       <BrowserRouter basename="/ReactMovieDB">
        <Routes>
            <Title />
-           <Switch>
+           
 
              
              <Route path="/" element={ <Home /> } />
-           </Switch>
+           
            <Footer />
        </Routes>
        </BrowserRouter>
