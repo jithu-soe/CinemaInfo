5f77d4347dfadd78ec9973a202913dec2bf2f714
jithu-soe <jithusoe@gmail.com>
2022-07-13 11:14:18 +0530

    fineshed

---
+++ src/Title.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+const Title = () => {
+return (
+
+<div className="apptitle">
+      
+      <span className="app1">C</span>
+      <span className="app2">I</span>
+      <span className="app3">N</span>
+      <span className="app4">E</span>
+      <span className="app5">M</span>
+      <span className="app6">A</span>
+      <span >                 </span>
+      <span className="app7">I</span>
+      <span className="app8">N</span>
+      <span className="app9">F</span>
+      <span className="app0">O</span>
+     
+      
+      
+    
+  </div>
+   );
+
+};
+
+export default Title;
