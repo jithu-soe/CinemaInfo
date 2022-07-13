dee5142ae0111052b9b15865dc9943b2deb38e57
jithu-soe <jithusoe@gmail.com>
2022-07-13 12:58:11 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import React from "react";
 import { BrowserRouter, Route,Routes} from 'react-router-dom';
 
-import Title from "./Title.js"
-import Footer from "./Footer.js"
+
+
 import Home from "./Home.js";
 
 
